c2be6ddcebc2992e2a9393eae5ab46f289115bff
Vikas-brainmoto <vikas@brainmoto.in>
2026-04-10 10:13:29 +0530

    Add files via upload

---
+++ schools.js
@@ -0,0 +1,56 @@
+const schoolsConfig = {
+            'default': {
+                name: "Your School",
+                fee: 0,
+                batches: "Batches assigned after registration",
+                logo: "" 
+            },
+            'bm_oea_pn1iu': { 
+                name: "Octominds Preschool Ambazari",
+                fee: 5100,
+                batches: "Mon & Thu: 3:00 PM - 4:00 PM",
+                logo: "assets/images/Octominds_logo.webp"
+            },
+             'bm_oefc_pn2kh': { 
+                name: "Octominds Preschool Friends Colony",
+                fee: 5100,
+                batches: "Mon & Thu: 3:00 PM - 4:00 PM",
+                logo: "assets/images/Octominds_logo.webp"
+            },
+             'bm_oej_pn3hu': { 
+                name: "Octominds Preschool Jaripatka",
+                fee: 5100,
+                batches: "Mon & Thu: 3:00 PM - 4:00 PM",
+                logo: "assets/images/Octominds_logo.webp"
+            },
+             'bm_oek_pn4sd': { 
+                name: "Octominds Preschool Khapri",
+                fee: 5100,
+                batches: "Mon & Thu: 3:00 PM - 4:00 PM",
+                logo: "assets/images/Octominds_logo.webp"
+            },
+             'bm_oels_pn5gh': { 
+                name: "Octominds Preschool London Street",
+                fee: 5100,
+                batches: "Batches assigned after registration",
+                logo: "assets/images/Octominds_logo.webp"
+            },
+             'bm_oemn_pn6dr': { 
+                name: "Octominds Preschool Manish Nagar",
+                fee: 5100,
+                batches: "Mon & Thu: 3:00 PM - 4:00 PM",
+                logo: "assets/images/Octominds_logo.webp"
+            },
+             'bm_oetn_pn7cg': { 
+                name: "Octominds Preschool Trimurti Nagar",
+                fee: 5100,
+                batches: "Mon & Thu: 3:00 PM - 4:00 PM",
+                logo: "assets/images/Octominds_logo.webp"
+            },
+            'bm_sssss_sk1ps': { 
+                name: "Sri Swami Senior Secondry School",
+                fee: 10,
+                batches: "Batches assigned after registration",
+                logo: "https://via.placeholder.com/150x50?text=Sri Swamy"
+            }
+        };
