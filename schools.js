4bd57b98de2fce823aebd70e04e8d5711fbd4aee
Vikas-brainmoto <vikas@brainmoto.in>
2026-04-11 01:43:57 +0530

    Add files via upload

---
+++ schools.js
@@ -0,0 +1,29 @@
+const schoolsConfig = {
+            'default': {
+                name: "Your Society",
+                fee: 0,
+                regularFee: 0,
+                batches: "Batches assigned after registration",
+                logo: "" 
+            },
+            'bm_isp_pn1ab': { 
+                name: "VTP HiLife",
+                fee: 2500,
+                regularFee: 3300,
+                batches: "Batches assigned after registration",
+                logo: "assets/images/vtphilife1.webp"
+            
+            },
+             'bm_oetn_pn7cg': { 
+                name: "Octominds Preschool Trimurti Nagar",
+                fee: 5100,
+                batches: "Mon & Thu: 3:00 PM - 4:00 PM",
+                logo: "assets/images/Octominds_logo.webp"
+            },
+            'bm_sssss_sk1ps': { 
+                name: "Sri Swami Senior Secondry School",
+                fee: 10,
+                batches: "Batches assigned after registration",
+                logo: "https://via.placeholder.com/150x50?text=Sri Swamy"
+            }
+        };
